855cc09b123bc7ca64ed5c3a9a591b624f048816
yogesh1663 <yogesh.code23@gmail.com>
2024-02-11 11:07:43 +0545

    fake data added to articles table using faker in ArticleFactory

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Nette\Utils\Random;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
@@ -17,7 +19,14 @@ class ArticleFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(),
+            'slug' => fake()->slug(),
+            'content' => fake()->paragraph(10),
+            'image' => fake()->image('public/storage/image/', 400, 300, null, false),
+            'views' => fake()->numberBetween(0, 100),
+            'status' => fake()->randomElement(['active', 'inactive']),
+            'category_id' => Category::get()->random()->id,
+
         ];
     }
 }
